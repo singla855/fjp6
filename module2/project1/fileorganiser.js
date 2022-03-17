10a79fc11b1c0002e296694d506eb9d82f636c07
Gaurang Singla <singlagaurang855@gmail.com>
2022-03-17 22:40:55 +0530

    filenames

--- module2/project1/fileorganiser.js
+++ module2/project1/fileorganiser.js
@@ -1,18 +1,64 @@
-// let inputarray=process.argv;
-// console.log(inputarray);
-// let input=inputarray[2];
-// console.log(input);
-let folderPath=process.argv[2];
-// console.log(folderpath);
+// // let inputarray=process.argv;
+// // console.log(inputarray);
+// // let input=inputarray[2];
+// // console.log(input);
+// let folderPath=process.argv[2];
+// // console.log(folderpath);
+// let fs = require("fs");
+// let folderExists = fs.existsSync(folderPath);
+
+// if(folderExists){
+//     //we will code
+//     // console.log("path is valid!!!!!!");
+//     let files = fs.readdirSync(folderPath);
+//     console.log(files);
+// }
+// else{
+//     console.log("Please Enter a Valid Path!!!!!!!");
+// }
+
+
 let fs = require("fs");
+let path = require("path");
+let folderPath = process.argv[2];
+
+// console.log(folderPath);
 let folderExists = fs.existsSync(folderPath);
+
+
+let extensions = {
+    Audio:[".mp3"],
+    Video:[".mp4",".mkv"],
+    Document:[".doc",".xlsx",".pdf",".txt"],
+    Image:[".jpeg",".jpg",".png",".gif"],
+    Software:[".exe"]
+};
+
 
 if(folderExists){
     //we will code
     // console.log("path is valid!!!!!!");
     let files = fs.readdirSync(folderPath);
-    console.log(files);
+    for(let i=0;i<files.length;i++){
+        let ext = path.extname(files[i]);
+        let nameOfFolder = giveFolderName(ext);
+        console.log("Ext--",ext,"Folder--",nameOfFolder);
+    }
+    
 }
 else{
     console.log("Please Enter a Valid Path!!!!!!!");
+}
+
+
+function giveFolderName(ext){
+    for(let key in extensions){
+        let extArr = extensions[key];
+        for(let i=0;i<extArr.length;i++){
+            if(extArr[i] == ext){
+                return key;
+            }
+        }
+    }
+    return 'Others'
 }
