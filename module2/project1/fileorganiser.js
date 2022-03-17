a760ca2dacdb1f6ea40b03ef9d48cff7c2fd1905
Gaurang Singla <singlagaurang855@gmail.com>
2022-03-17 23:24:37 +0530

    project completed

--- module2/project1/fileorganiser.js
+++ module2/project1/fileorganiser.js
@@ -35,6 +35,34 @@ let extensions = {
 };
 
 
+// if(folderExists){
+//     //we will code
+//     // console.log("path is valid!!!!!!");
+//     let files = fs.readdirSync(folderPath);
+//     for(let i=0;i<files.length;i++){
+//         let ext = path.extname(files[i]);
+//         let nameOfFolder = giveFolderName(ext);
+//         console.log("Ext--",ext,"Folder--",nameOfFolder);
+//     }
+    
+// }
+// else{
+//     console.log("Please Enter a Valid Path!!!!!!!");
+// }
+
+
+// function giveFolderName(ext){
+//     for(let key in extensions){
+//         let extArr = extensions[key];
+//         for(let i=0;i<extArr.length;i++){
+//             if(extArr[i] == ext){
+//                 return key;
+//             }
+//         }
+//     }
+//     return 'Others'
+// }
+
 if(folderExists){
     //we will code
     // console.log("path is valid!!!!!!");
@@ -42,9 +70,16 @@ if(folderExists){
     for(let i=0;i<files.length;i++){
         let ext = path.extname(files[i]);
         let nameOfFolder = giveFolderName(ext);
-        console.log("Ext--",ext,"Folder--",nameOfFolder);
+        // console.log("Ext--",ext,"Folder--",nameOfFolder);
+        let pathOfFolder = path.join(folderPath,nameOfFolder); 
+        let exist = fs.existsSync(pathOfFolder);
+        if(exist){
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }else{
+            fs.mkdirSync(pathOfFolder);
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }
     }
-    
 }
 else{
     console.log("Please Enter a Valid Path!!!!!!!");
@@ -61,4 +96,12 @@ function giveFolderName(ext){
         }
     }
     return 'Others'
+}
+
+
+function moveFile(folderPath,pathOfFolder,fileName){
+    let sourcePath = path.join(folderPath,fileName);
+    let destinationPath = path.join(pathOfFolder,fileName);
+    fs.copyFileSync(sourcePath,destinationPath);
+    fs.unlinkSync(sourcePath);
 }
