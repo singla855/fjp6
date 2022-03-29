3c0450d4d596ff38685f7d212341cad4fcb526c4
Gaurang Singla <singlagaurang855@gmail.com>
2022-03-29 23:05:35 +0530

    activity1 of hwt

--- module2/web_scrapping/jsdom.js
+++ module2/web_scrapping/jsdom.js
@@ -1,6 +1,5 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-
-
 const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+
 console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
