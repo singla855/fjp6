9789fe0e2bdb59ad9c4b06421eb03f7d5f007aa3
Gaurang Singla <singlagaurang855@gmail.com>
2022-03-31 23:42:13 +0530

    leaderboard details

--- module2/web_scrapping/leaderboard.js
+++ module2/web_scrapping/leaderboard.js
@@ -4,6 +4,8 @@ const { JSDOM } = jsdom;
 
 const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 
+let leaderboard = [];
+
 request(link,cb);
 
 function cb(error,response,html){
@@ -16,7 +18,61 @@ function cb(error,response,html){
         for(let i=0;i<allScorecardTags.length;i++){
             let link = allScorecardTags[i].href;
             let completeLink = "https://www.espncricinfo.com"+link;
-            console.log(completeLink);
+            // console.log(completeLink);
+            request(completeLink,cb2);
         }
     }
+}
+
+function cb2(error,response,html){
+    if(error){
+        console.log(error);
+    }else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let batsmenRow = document.querySelectorAll(".table.batsman tbody tr");
+        for(let i=0;i<batsmenRow.length;i++){
+            let cells = batsmenRow[i].querySelectorAll("td");
+            if(cells.length == 8){
+                let name = cells[0].textContent;
+                let runs = cells[2].textContent;
+                let balls = cells[3].textContent;
+                let fours = cells[5].textContent;
+                let sixes = cells[6].textContent;
+                console.log("Name : ",name,"Runs : ",runs,"Balls : ",balls,"Fours : ",fours,"Sixes : ",sixes);
+            }
+        }
+    }
+}
+
+processPlayer('Rohit','15','4','2','4');
+processPlayer('Virat','50','20','4','3')
+processPlayer('Rohit','40','20','1','2');
+console.log(leaderboard);
+
+function processPlayer(name,runs,balls,fours,sixes){
+    runs = Number(runs);
+    balls = Number(balls);
+    fours = Number(fours);
+    sixes = Number(sixes);
+    for(let i=0;i<leaderboard.length;i++){
+        let playerObj = leaderboard[i];
+        if(playerObj.Name == name){
+            //will do some work here
+            playerObj.Runs+=runs;
+            playerObj.Balls+=balls;
+            playerObj.Fours+=fours;
+            playerObj.Sixes+=sixes;
+            return;
+        }
+    }
+    // code coming here means we did not find our player inside leaderboard
+    let obj = {
+        Name:name,
+        Runs:runs,
+        Balls:balls,
+        Fours:fours,
+        Sixes:sixes
+    }
+    leaderboard.push(obj);
 }
